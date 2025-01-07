01d754a20b676948df6ad2313396749ecf06e8bc
sudheerthi <gunasaisudheerthi@gmail.com>
2025-01-07 15:36:07 +0530

    15th program added

--- prgm15.js
+++ prgm15.js
@@ -1,1 +1,18 @@
-{name: "John", age: 25, primaryInterest: "Reading", secondaryInterest: "Traveling"}
+const user = {
+    name: 'John',
+    age: 25,
+    interests: ['Reading', 'Traveling', 'Cooking']
+};
+function createProfile({ name, age, interests }) {
+
+    const [primaryInterest, secondaryInterest] = interests;
+
+    return {
+        name,
+        age,
+        primaryInterest,
+        secondaryInterest
+    };
+}
+
+console.log(createProfile(user));
