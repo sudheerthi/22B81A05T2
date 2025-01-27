bdb9ffc296f15fc77c7f1357bfb79c7eb1ce1ea2
sudheerthi <gunasaisudheerthi@gmail.com>
2025-01-27 20:01:47 +0530

    Delete prgm3.js

--- prgm3.js
+++
@@ -1,7 +0,0 @@
-const employee = {name: "Afroz", role: "Developer", age: 28, location: "IN"}
-
-function updateEmpDetails(employee,newrole){
-    return {...employee,role:newrole};
-}
-
-console.log(updateEmpDetails(employee,"Manager"))
