399dd7d73fde9746e3ef22d5fdf914f8b685e2a9
sudheerthi <gunasaisudheerthi@gmail.com>
2025-01-27 20:08:03 +0530

    Create 1.js

---
+++ lab3/1.js
@@ -0,0 +1,33 @@
+let x=20;
+console.log(x);
+
+x=10;
+console.log(x + " "+typeof x);
+
+let name="abhi";
+console.log(name +" "+typeof name);
+
+let crct=true;
+console.log(crct+" "+typeof crct);
+
+//var
+
+console.log(y);
+var y=20;
+console.log(y);
+y=10;
+console.log(y + " "+typeof y);
+var name1 ="jai";
+console.log(name1 +" "+typeof name1);
+var crct1=false;
+console.log(crct1+" "+typeof crct1);
+
+//const
+const z=30;
+console.log(z);
+console.log(typeof z);
+
+const name2="raghu";
+console.log(name2+" "+typeof name2);
+const crct2=true;
+console.log(crct2+" "+typeof crct2);
